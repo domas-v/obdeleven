5298fda4495eb5e89709472ca4cee165ca32a9df
domas-v <domas.vaitmonas93@gmail.com>
2024-03-17 18:18:24 +0200

    Add CORS middleware and logging to API endpoint, use LLEL

--- api.py
+++ api.py
@@ -1,14 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uvicorn
+import logging
 
-from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_community.chat_models.huggingface import ChatHuggingFace
 from langchain.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 from config import DB, RETRIEVER, LLM, POEM_DOCS, TEMPLATE
 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Query(BaseModel):
@@ -16,24 +35,29 @@ class Query(BaseModel):
 
 
 @app.post("/ask/")
-def ask(query: Query):
-    # question = "Pacituok lietuvių liaudies eilėraštį 'Du gaideliai'"
-    question = query.question
+def ask(query: Query) -> dict:
+    logger.info(f"Question: {query.question}")
 
     if DB._collection.count() == 0:
+        logger.info("Document not found. Adding documents to the database")
         DB.add_documents(POEM_DOCS)
 
     prompt = PromptTemplate(template=TEMPLATE, input_variables=["context", "query"])
-    chain = RetrievalQA.from_chain_type(
-        llm=ChatHuggingFace(llm=LLM),
-        retriever=RETRIEVER,
-        verbose=True,
-        return_source_documents=True,
-        chain_type_kwargs={"prompt": prompt},
+    logger.info(f"Prompt: {prompt.template}")
+
+    llm = ChatHuggingFace(llm=LLM)
+    chain = (
+        {"context": RETRIEVER, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
     )
 
-    answer = chain({"query": question})
-    return {"answer": answer["result"]}
+    logger.info("Running the chain")
+    answer = chain.invoke(query.question)
+    logger.info(f"Answer: {answer}")
+
+    return {"answer": answer}
 
 
 if __name__ == "__main__":
